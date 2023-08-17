31ff0c3b7fbc7a66b10edcf45e90b337408001db
Dawin <dawinyurtseven@gmail.com>
2023-08-17 19:16:05 +0200

    updato!!

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
 function toggleMenu() {
     var width = document.getElementById("collapseMenu").style.width;
-    if (width === "250px") {
+    if (width === "350px") {
         document.getElementById("collapseMenu").style.width = "0px";
         document.getElementById("MenuButton").style.right = "10px";
         document.body.style.backgroundColor = "white";
     }
     else {
-        document.getElementById("collapseMenu").style.width = "250px";
-        document.getElementById("MenuButton").style.right = "260px";
+        document.getElementById("collapseMenu").style.width = "350px";
+        document.getElementById("MenuButton").style.right = "360px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
 }

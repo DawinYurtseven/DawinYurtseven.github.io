b85441e6a9e6d9b55289d736d85989b55d56d45e
Dawin <dawinyurtseven@gmail.com>
2023-08-17 18:37:59 +0200

    stuff 2

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-function toggleMenu(){
+function toggleMenu() {
     var width = document.getElementById("collapseMenu").style.width;
-    if(width === "250px"){
+    if (width === "250px") {
         document.getElementById("collapseMenu").style.width = "0px";
         document.getElementById("MenuButton").style.right = "10px";
         document.body.style.backgroundColor = "white";
     }
-    else{
+    else {
         document.getElementById("collapseMenu").style.width = "250px";
         document.getElementById("MenuButton").style.right = "260px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";

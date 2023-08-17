36c312d87ea0966edc8fe05ded600030fcb5739c
Dawin <dawinyurtseven@gmail.com>
2023-08-17 16:46:06 +0200

    update menu

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
-function nacCollaps(){
-    var x = document.getElementById(collapseMenu);
-    if(x.className === "innerMenu") {
-        x.className += " responsive";
-    }
-    else{
-        x.className = "innerMenu";
-    }
+function openNav(){
+    document.getElementById("innerMenu").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav(){
+    document.getElementById("innerMenu").style.width = "0px";
+    document.body.style.backgroundColor = "white";
 }

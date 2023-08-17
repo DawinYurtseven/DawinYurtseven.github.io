15f728e4d3a0c8ae61146f26e60a6deb957d0c4a
Dawin <dawinyurtseven@gmail.com>
2023-08-17 16:49:42 +0200

    update

--- functions.js
+++ functions.js
@@ -1,9 +1,11 @@
-function openNav(){
-    document.getElementById("innerMenu").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav(){
-    document.getElementById("innerMenu").style.width = "0px";
-    document.body.style.backgroundColor = "white";
+function toggleMenu(){
+    var width = document.getElementById("collapseMenu").style.width;
+    if(width === "250px"){
+        document.getElementById("collapseMenu").style.width = "0px";
+        document.body.style.backgroundColor = "white";
+    }
+    else{
+        document.getElementById("collapseMenu").style.width = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
 }

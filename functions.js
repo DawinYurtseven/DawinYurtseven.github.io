fbc1fe4c6c4ca363505c95181fb27e14366f2ddc
Dawin <dawinyurtseven@gmail.com>
2023-08-20 10:37:39 +0200

    to newest state

--- functions.js
+++ functions.js
@@ -4,11 +4,17 @@ function toggleMenu() {
         document.getElementById("collapseMenu").style.width = "0px";
         document.getElementById("MenuButton").style.right = "10px";
         document.body.style.backgroundColor = "white";
+        document.getElementById("wrapper").style.opacity = 1;
         
     }
     else {
         document.getElementById("collapseMenu").style.width = "350px";
         document.getElementById("MenuButton").style.right = "360px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        document.getElementById("wrapper").style.opacity = 0.7;
     }
+}
+
+function onHoverImage(){
+    
 }
